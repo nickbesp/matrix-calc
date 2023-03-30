943e3864f148da664a8453f5c12298215e864ebf
Bespyatykh Nikita <78490935+nickbesp@users.noreply.github.com>
2023-03-30 20:54:44 +0500

    Update laba4.py

--- laba4.py
+++ laba4.py
@@ -1,25 +1,25 @@
-#Создание матрицы
+#creating a matrix
 def create():
-    print('Введите размеры матрицы через пробел:')
+    print('Type the size of matrix separated with space:')
     m, n = map(int, input().split())
     mat = list()
-    print(f'Введите строку матрицы длины {n} через пробел {m} раз(а):')
+    print(f"Type the matrix's row {m} times with length {n} using space:")
     for i in range(m):
         row = list(map(int, input().split()))
         mat.append(row)
-    print('Матрица создана!')
+    print('The Matrix is created!')
     return mat
 
-#Красивый вывод матрицы
+#pretty matrix print
 def printMat(matrix):
     for i in matrix:
         print(*i)
 
-#Сложение матриц
+#the sum of two matrices
 def sumMat(mat1, mat2):
 
     if len(a) != len(b) or len(a[0]) != len(b[0]):
-        raise ValueError("Matrixes' sizes should be equal!")
+        raise ValueError("Matrices' sizes should be equal!")
     
     result = list()
     for i in range(len(a)):
@@ -30,7 +30,7 @@ def sumMat(mat1, mat2):
         
     return result
 
-#Умножение матрицы на число
+#matrix multiplication by a number
 def multMatNum(matrix, num):
     result = list()
     for i in range(len(matrix)):
@@ -40,7 +40,7 @@ def multMatNum(matrix, num):
         result.append(row)
     return result
 
-#Произведение матриц
+#multiplication of two matrices
 def multMat(m1, m2):
     r = []
     m = []
@@ -58,7 +58,7 @@ def multMat(m1, m2):
         r = []
     return m
 
-#Транспонирование матриц
+#transposition of the matrix
 def transpos(matrix):
     #Создание пустой матрицы jxi (изначально у нас ixj)
     result = list()
@@ -73,7 +73,7 @@ def transpos(matrix):
             result[j][i] = matrix[i][j]
     return result
 
-#Определитель матрицы
+#determinant of the matrix
 def det(matrix):
 
     if len(matrix) != len(matrix[0]):
@@ -82,7 +82,7 @@ def det(matrix):
     if len(matrix) == 2:
         return minor(matrix)
     
-    #Правило Лапласа
+    #Laplace's rule (I don't sure the name is correct)
     summ = 0
     for j in range(len(matrix)):
         elem = matrix[0][j]
@@ -90,12 +90,12 @@ def det(matrix):
 
     return summ
 
-#Алгоритмическое дополнение элемента матрицы
+#algebraic complement of a matrix element
 def adding(matrix, i, j):
     if len(matrix) == 2:
         return(minor(matrix) * (-1)**(i + j))
 
-    #Создание матрицы на порядок меньше, для которой будет считаться минор
+    #creating a matrix an order of magnitude smaller than the one you pass to the func
     new_mat = list()
     for q in range(len(matrix[0])):
         if q == i:
@@ -109,17 +109,17 @@ def adding(matrix, i, j):
 
     return det(new_mat) * (-1)**(i + j)
 
-#Минор к элементу матрицы 2x2
+#a minor or a determinant of a matrix 2x2
 def minor(matrix):
     return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
 print('\n')
-print('Что вы хотите сделать? (0 - выйти)')
-print('1 - сложение матриц')
-print('2 - умножение матрицы на число')
-print('3 - произведение матриц')
-print('4 - транспортирование матриц')
-print('5 - определитель матрицы')
+print('what do you want to calc? (0 - exit)')
+print('1 - the sum of two matrices')
+print('2 - matrix multiplication by a number')
+print('3 - multiplication of two matrices')
+print('4 - transposition of the matrix')
+print('5 - determinant of the matrix')
 action = int(input())
 
 while action != 0:
@@ -127,33 +127,33 @@ while action != 0:
 
     if action == 1:
         mat2 = create()
-        print('\n Ответ:')
+        print('\n Answer:')
         printMat(sumMat(mat1, mat2))
 
     if action == 2:
-        print('Введите множитель:')
+        print('Enter the multiplier:')
         num = int(input())
-        print('\n Ответ:')
+        print('\n Answer:')
         printMat(multMatNum(mat1, num))
 
     if action == 3:
         mat2 = create()
-        print('\n Ответ:')
+        print('\n Answer:')
         printMat(multMat(mat1, mat2))
 
     if action == 4:
-        print('\n Ответ:')
+        print('\n Answer:')
         printMat(transpos(mat1))
 
     if action == 5:
-        print('\n Ответ:')
+        print('\n Answer:')
         print(det(mat1))
 
     print('\n')
-    print('Что вы хотите сделать? (0 - выйти)')
-    print('1 - сложение матриц')
-    print('2 - умножение матрицы на число')
-    print('3 - произведение матриц')
-    print('4 - транспортирование матриц')
-    print('5 - определитель матрицы')
+    print('what do you want to calc? (0 - exit)')
+    print('1 - the sum of two matrices')
+    print('2 - matrix multiplication by a number')
+    print('3 - multiplication of two matrices')
+    print('4 - transposition of the matrix')
+    print('5 - determinant of the matrix')
     action = int(input())
